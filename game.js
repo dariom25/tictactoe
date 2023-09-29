fe5f07740ecadc8b0071ff1e150f7fc0093b1785
dariom25 <dario@maciey.de>
2023-09-29 20:44:44 +0200

    Add function to remove the input fields

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ const gameBoard = (() => {
         board = [];
     };
 
-    const setToken = (field, token) => {
+    const setToken = (field, token) => { //if else counter -1 dann?
         if (gameBoard.board[field] === undefined) {
             gameBoard.board[field] = token;
         }
@@ -37,7 +37,7 @@ const gameBoard = (() => {
         } else if (board[2] === token && board[4] === token && board[6] === token) {
             alert("Win");
             emptyBoard();
-        } else if (board.length === 8 && board.includes("undefined") !== true) {
+        } else if (board.length === 8 && board.includes("undefined") !== true) { // ist korrekt, aber noch nicht schön. tie wird vor letztem zug gezeigt; Noch verbuggt!!!
             alert("Tie");
             emptyBoard();
         }
@@ -56,7 +56,7 @@ const gameController = (() => {
 
     const playRound = (field, player1, player2) => {
         let player = checkWhichPlayersTurnItIs(player1, player2);
-        gameBoard.setToken(field, player.token); //wenn ein belegtes feld angeklickt wird, wird playRound und der Counter trotzdem zu Ende ausgeführt!
+        gameBoard.setToken(field, player.token); //wenn ein belegtes feld angeklickt wird, wird playRound und der Counter trotzdem zu Ende ausgeführt! 
         gameBoard.checkForWin(player.token);
         increaseRoundCounter();
     };
@@ -69,19 +69,21 @@ const gameController = (() => {
         }
     }
 
-    return {playRound, checkWhichPlayersTurnItIs, round}
+    return {playRound, checkWhichPlayersTurnItIs}
 })();
 
-const Player = (playerName, token) => {
-    return {playerName, token}
+const Player = (name, token) => {
+    return {name, token}
 }
 
 
 const displayController = (() => {
     const fields = document.querySelectorAll(".field");
     const startBtn = document.querySelector(".start-button");
-    const player1Name = document.getElementById("player1").value;
-    const player2Name = document.getElementById("player2").value;
+    const player1Name = document.getElementById("player1");
+    const player2Name = document.getElementById("player2");
+    const inputPlayer1Parent = document.querySelector("#player1-container");
+    const inputPlayer2Parent = document.querySelector("#player2-container");
     let player1;
     let player2;
 
@@ -89,12 +91,22 @@ const displayController = (() => {
     const displayToken = (field, player1, player2) => {
         let player = gameController.checkWhichPlayersTurnItIs(player1, player2);
         field.textContent = player.token;
+    };
+
+    const removeInputFields = () => {
+        inputPlayer1Parent.removeChild(player1Name);
+        inputPlayer2Parent.removeChild(player2Name);
+    }
+
+    const displayPlayerName = (player1, player2) => {
+        removeInputFields();
+
     }
 
     startBtn.addEventListener("click", (event) => {
         event.preventDefault();
-        player1 = Player(player1Name, "X");
-        player2 = Player(player2Name, "O");
+        player1 = Player(player1Name.value, "X");
+        player2 = Player(player2Name.value, "O");
     });
 
     fields.forEach((field) => {
@@ -104,5 +116,7 @@ const displayController = (() => {
             displayToken(field, player1, player2); //X und O werden überschrieben wenn feld schon belegt ist; s.O.
         });
     })
+
+    return {player1, player1Name}
 })();
 
